ed71aced383b555f5de86f022addc3ff6bb9bca7
Urgyán Bálint Viktor <urgyanbalintviktor@gmail.com>
2026-05-23 18:09:50 +0200

    feat: add score service layer

---
+++ src/main/java/com/miniarcade/score_service/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.miniarcade.score_service.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
